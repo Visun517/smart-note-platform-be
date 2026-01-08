7c67d49d9c856d87ce9414b5e50763497471b023
Visun517 <visunpraboda999@gmail.com>
2026-01-09 00:57:21 +0530

    depled-update

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -228,8 +228,10 @@ export const googleLogin = async (req: Request, res: Response) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      // secure: process.env.NODE_ENV === "production",
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
